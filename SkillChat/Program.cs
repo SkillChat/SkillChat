8ce6b60368747d7d4f9d12efac5fadfc0457d68d
Kibnet Philosoff <kibnet@hotmail.com>
2020-05-11 14:37:47 +0300

    Пустой проект

---
+++ SkillChat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkillChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
