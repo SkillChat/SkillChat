891dba4f1b07a03dbeb4756e6a6f8bf7e6d4ac27
Kozlov-ae <allek-007@list.ru>
2020-12-07 06:48:15 +0300

    refactor: Убрал отправку сообщения по enter из кодБихаенда feat: Автофокус на поле ввода после отправки сообщения нажатием на кнопку отправить.

---
+++ SkillChat.Client/Extentions/TextBoxInputMessageBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillChat.Client.Extentions
+{
+	public class TextBoxInputMessageBehavior : Behavior<TextBox>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			AssociatedObject.KeyDown += NewLineMethod;
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			AssociatedObject.KeyDown -= NewLineMethod;
+		}
+
+		/// <summary>Переопределение стандартного поеведения по нажатиям на клавиши клавиатуры у поля ввода сообщений</summary>
+		/// <param name="sender">Инициатор (ТекстБокс)</param>
+		/// <param name="e">Переданные параметры</param>
+		public void NewLineMethod(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter && e.KeyModifiers == KeyModifiers.Control)
+			{
+				AssociatedObject.Text += Environment.NewLine;
+			}
+		}
+	}
+}
