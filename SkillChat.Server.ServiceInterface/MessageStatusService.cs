110845480ef6fd82fa7a8f59baa067a580dabe6a
Kozlov-ae <allek-007@list.ru>
2021-02-09 18:44:48 +0300

    Создал модель статусов сообщений для АПИ

---
+++ SkillChat.Server.ServiceInterface/MessageStatusService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ServiceStack;
+using SkillChat.Server.ServiceModel.Molds;
+
+namespace SkillChat.Server.ServiceInterface
+{
+    public class MessageStatusService : Service
+    {
+        public async Task Post(MessageStatusCollectionMold statuses)
+        {
+
+        }
+    }
+}
