9ff7f96d9079a3897a8c2c02f7097aceb995b1b7
Kozlov-ae <allek-007@list.ru>
2021-02-10 07:59:27 +0300

    Создал доменную модель

---
+++ SkillChat.Server.Domain/MessageStatusDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace SkillChat.Server.Domain
+{
+    /// <summary>Хранилище статусов сообщений</summary>
+    public class MessageStatusDomain
+    {
+        public string MessageId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset ReadDate { get; set; }
+        public DateTimeOffset ReceivedDate { get; set; }
+    }
+}
