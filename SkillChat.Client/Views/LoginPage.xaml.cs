48b084b8c191b1d2bfa89e1eaa9b25514862f2fb
EgorTrubnikov <navakishor.108@gmail.com>
2021-09-11 22:26:03 +0300

    fix/#81-LoginPage and RegisterPage - перенёс стили в отдельный файл. Убрал логику из код бихайнд.

--- SkillChat.Client/Views/LoginPage.xaml.cs
+++ SkillChat.Client/Views/LoginPage.xaml.cs
@@ -13,38 +13,11 @@ namespace SkillChat.Client.Views
         public LoginPage()
         {
             this.InitializeComponent();
-            this.DataContextChanged += ChangeStyles;
-            controlsStyles = new List<Control>
-            {
-                this.Get<Border>("PasswordBr"),
-                this.Get<Border>("LoginBr"),
-                this.Get<TextBox>("LoginTbx"),
-                this.Get<TextBox>("PasswordTB"),
-                this.Get<TextBlock>("LoginTB"),
-                this.Get<TextBlock>("PasswordTBL")
-            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-        /// <summary>Меняем стили</summary>
-        private void ChangeStyles(object sender, EventArgs e)
-        {
-            if (this.DataContext is MainWindowViewModel vm)
-            {
-                vm.ErrorBe += () => {
-                   foreach(var cont in controlsStyles){ cont.Classes.Set("Error", true);}
-                };
-                vm.ResetError += () =>
-                {
-                    foreach (var cont in controlsStyles) { cont.Classes.Remove("Error"); }
-                };
-            }
-        }
-
-        //Коллекция контролов для изменения стилей
-        List<Control> controlsStyles;
     }
 }
